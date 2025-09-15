9682d9293dc33c471cd77b16652760830670ae08
katheODOS <katherine@odostech.com>
2025-09-15 12:06:19 +0200

    changed path for making tiff predictions for images

--- outputs/biodiversity_tiff_test_direct.py
+++ outputs/biodiversity_tiff_test_direct.py
@@ -1,3 +1,9 @@
+
+import sys
+import os
+
+# Add the parent directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import ttach as tta
 import multiprocessing.pool as mpp
 import multiprocessing as mp
